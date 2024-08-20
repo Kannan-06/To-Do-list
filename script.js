cbae0cdec3c03600735a19cacfeca27bf4c9a73d
Kannan P <pkannan1824@gmail.com>
2024-08-20 08:27:52 +0530

    Finalize data

--- script.js
+++ script.js
@@ -14,12 +14,24 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = '';
+    saveData()
 }
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagname === 'Li'){
+    if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData()
     }
-    else li(e.target.tagname === 'SPAN'){
+    else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 },false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask()
